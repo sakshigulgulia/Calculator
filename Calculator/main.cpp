5bc5ec47b8f92e667ea0c919757704431783c7bb
sakshigulgulia <sakshigulgulia1999@gmail.com>
2022-08-22 21:45:42 -0400

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Sakshi Gulgulia on 8/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
